7815075fc62928fd41099e242b7ee55895b8e1fa
xuyuexiong <530969335@qq.com>
2018-07-24 15:49:31 +0800

    update

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,29 @@
 function Vue(options) {
-  var self = this;
-  this.data = options.data;
-  this.methods = options.methods;
-
-  Object.keys(this.data).forEach(function (key) {
-    self.proxyKeys(key);
-  });
-
-  observe(this.data);
-  new Compile(options.el, this);
-  options.mounted.call(this); //所有事情处理好后执行mounted函数
+	var self = this;
+	this.data = options.data;
+	this.methods = options.methods;
+	//vm实例对data操作
+	Object.keys(this.data).forEach(function (key) {
+		self.proxyKeys(key);
+	});
+	//监听data
+	observe(this.data);
+	new Compile(options.el, this);
+	options.mounted.call(this); //所有事情处理好后执行mounted函数
 }
 
 Vue.prototype = {
-  proxyKeys: function (key) {
-    var self = this;
-    Object.defineProperty(this, key, {
-      enumerable: false,
-      configurable: true,
-      get: function () {
-        return self.data[key];
-      },
-      set: function (newVal) {
-        self.data[key] = newVal;
-      },
-    });
-  },
+	proxyKeys: function (key) {
+		var self = this;
+		Object.defineProperty(this, key, {
+			enumerable: false,
+			configurable: true,
+			get: function () {
+				return self.data[key];
+			},
+			set: function (newVal) {
+				self.data[key] = newVal;
+			},
+		});
+	},
 };
